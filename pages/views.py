729b42ffc274dedd9ec79eb6c7101cca8b08f1a8
Vygunda <157414394+Vygunda@users.noreply.github.com>
2024-10-12 13:33:57 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.conf import settings
+from django.utils.http import urlsafe_base64_encode, force_bytes
 from .models import Contact, Invoice
 
 def index(request):
